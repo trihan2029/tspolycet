2edbe6b4e4c661f68bb678f9912d8838b6ab41c5
Trihan <trihan2029@gmail.com>
2025-05-05 07:52:07 +0530

    maths-2

---
+++ questions/mat.bat
@@ -0,0 +1,120 @@
+rename "1 (2).JPG" 61.jpg
+
+rename "2 (2).JPG" 62.jpg
+
+rename "3 (2).JPG" 63.jpg
+
+rename "4 (2).JPG" 64.jpg
+
+rename "5 (2).JPG" 65.jpg
+
+rename "6 (2).JPG" 66.jpg
+
+rename "7 (2).JPG" 67.jpg
+
+rename "8 (2).JPG" 68.jpg
+
+rename "9 (2).JPG" 69.jpg
+
+rename "10 (2).JPG" 70.jpg
+
+rename "11 (2).JPG" 71.jpg
+
+rename "12 (2).JPG" 72.jpg
+
+rename "13 (2).JPG" 73.jpg
+
+rename "14 (2).JPG" 74.jpg
+
+rename "15 (2).JPG" 75.jpg
+
+rename "16 (2).JPG" 76.jpg
+
+rename "17 (2).JPG" 77.jpg
+
+rename "18 (2).JPG" 78.jpg
+
+rename "19 (2).JPG" 79.jpg
+
+rename "20 (2).JPG" 80.jpg
+
+rename "21 (2).JPG" 81.jpg
+
+rename "22 (2).JPG" 82.jpg
+
+rename "23 (2).JPG" 83.jpg
+
+rename "24 (2).JPG" 84.jpg
+
+rename "25 (2).JPG" 85.jpg
+
+rename "26 (2).JPG" 86.jpg
+
+rename "27 (2).JPG" 87.jpg
+
+rename "28 (2).JPG" 88.jpg
+
+rename "29 (2).JPG" 89.jpg
+
+rename "30 (2).JPG" 90.jpg
+
+rename "31 (2).JPG" 91.jpg
+
+rename "32 (2).JPG" 92.jpg
+
+rename "33 (2).JPG" 93.jpg
+
+rename "34 (2).JPG" 94.jpg
+
+rename "35 (2).JPG" 95.jpg
+
+rename "36 (2).JPG" 96.jpg
+
+rename "37 (2).JPG" 97.jpg
+
+rename "38 (2).JPG" 98.jpg
+
+rename "39 (2).JPG" 99.jpg
+
+rename "40 (2).JPG" 100.jpg
+
+rename "41 (2).JPG" 101.jpg
+
+rename "42 (2).JPG" 102.jpg
+
+rename "43 (2).JPG" 103.jpg
+
+rename "44 (2).JPG" 104.jpg
+
+rename "45 (2).JPG" 105.jpg
+
+rename "46 (2).JPG" 106.jpg
+
+rename "47 (2).JPG" 107.jpg
+
+rename "48 (2).JPG" 108.jpg
+
+rename "49 (2).JPG" 109.jpg
+
+rename "50 (2).JPG" 110.jpg
+
+rename "51 (2).JPG" 111.jpg
+
+rename "52 (2).JPG" 112.jpg
+
+rename "53 (2).JPG" 113.jpg
+
+rename "54 (2).JPG" 114.jpg
+
+rename "55 (2).JPG" 115.jpg
+
+rename "56 (2).JPG" 116.jpg
+
+rename "57 (2).JPG" 117.jpg
+
+rename "58 (2).JPG" 118.jpg
+
+rename "59 (2).JPG" 119.jpg
+
+rename "60 (2).JPG" 120.jpg
+
